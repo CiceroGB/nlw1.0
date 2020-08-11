c55d5268a3c17055de0d1a7358d819a943b9c9cd
Cicero Brunelli <cicero.brunelli@gmail.com>
2020-08-11 13:46:53 -0300

    item controller

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,8 @@
 import express from 'express';
+import ItemsController from './controllers/itemController';
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-  res.json({ msg: 'oi' });
-});
+routes.get('/items', ItemsController.index);
 
 export default routes;
