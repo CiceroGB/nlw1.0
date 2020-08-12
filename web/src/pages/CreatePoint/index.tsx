23b05ed14c1360f6b673bd3ba7f8be055cd1505a
Cicero Brunelli <cicero.brunelli@gmail.com>
2020-08-11 23:36:04 -0300

    link home - create

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import {FiArrowLeft} from 'react-icons/fi';
+
+import logo from '../../assets/logo.svg';
 
 import './styles.css';
+import { Link } from 'react-router-dom';
 
 
 const CreatePoint = () => {
     return (
-        <h1>CreatePoint</h1>
+        <div id="page-create-point">
+            <header>
+                <img src={logo} alt="Ecoleta"></img>
+                <Link to ="/">
+                    <FiArrowLeft/>
+                    Voltar para home
+                </Link>
+            </header>
+
+
+        </div>
     )
 }
 
