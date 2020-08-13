6c091fb82f5b602925a83d4fcef59119861e94d4
Cicero Brunelli <cicero.brunelli@gmail.com>
2020-08-13 17:37:39 -0300

    home

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { View, ImageBackground, Image, Text } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler'
+import { Feather as Icon } from '@expo/vector-icons'
 
 import styles from './styles';
-
-
-
 
 
 const Home = () => {
@@ -16,15 +15,23 @@ const Home = () => {
         >
             <View style={styles.main}>
                 <Image source={require('../../assets/logo.png')} />
-
                 <Text style={styles.title}>
                     Seu Marketplace de coleta
                 </Text>
-
                 <Text style={styles.description}>
                     Ajudando a encontrar pontos de coleta
                 </Text>
+            </View>
 
+            <View style={styles.footer}>
+                <RectButton style={styles.button} onPress={() => { }}>
+                    <View style={styles.buttonIcon}>
+                        <Icon name="arrow-right" size={24} color="#FFF" />
+                    </View>
+                    <Text style={styles.buttonText}>
+                        Entrar
+                    </Text>
+                </RectButton>
             </View>
 
 
