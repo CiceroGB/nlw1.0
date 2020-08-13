59cc92a7c5a2384672a8a2a27cec585f18b2c5ef
Cicero Brunelli <cicero.brunelli@gmail.com>
2020-08-13 17:41:33 -0300

    navigation routes

---
+++ mobile/src/pages/Points/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+
+
+const Points = () => {
+    return(
+        <View>
+            Points
+        </View>
+    )
+}
+
+export default Points;
