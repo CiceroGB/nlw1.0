d70a7f3cd74fff69a03a8d069a98eceae375720f
Cicero Brunelli <cicero.brunelli@gmail.com>
2020-08-14 09:59:19 -0300

    navigation goback to home

--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
-import { View,Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { Feather as Icon } from '@expo/vector-icons'
+
+
+import styles from './styles';
+import { useNavigation } from '@react-navigation/native';
 
 
 const Points = () => {
-    return(
-        <View>
-            <Text>
-            Points
-            </Text>
-            
+    const navigation = useNavigation();
+
+
+    function handleNavigatrBack() {
+        navigation.goBack();
+    }
+
+    return (
+        < View style={styles.container}>
+            <TouchableOpacity onPress={handleNavigatrBack}>
+                <Icon name="arrow-left" size={25} />
+            </TouchableOpacity>
+
+
+
         </View>
     )
 }
